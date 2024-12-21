5d4d75ece1e3f8fd752959857be00082fb054fff
MilanReverendo <r0937873@student.thomas.be>
2024-12-21 16:59:23 +0100

    changes to api gateway

--- api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/SecurityConfig.java
@@ -1,13 +1,10 @@
 package fact.it.apigateway.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -17,16 +14,9 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         serverHttpSecurity
                 .authorizeExchange(exchange ->
-                        // Permit public access to specific paths
-                        exchange.pathMatchers(HttpMethod.GET, "/events", "/users/register")  // Public routes
-                                .permitAll()
-                                // Any other request requires authentication
-                                .anyExchange()
-                                .authenticated()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(withDefaults())  // Configure JWT authentication
-                );
+                        // Make all routes public by permitting all requests
+                        exchange.anyExchange().permitAll()
+                );// You can remove this line if no authentication is needed
         return serverHttpSecurity.build();
     }
 }
